370e4b70aa860c955957dbac451f74fa31078923
RebekahPerkins <r.perkins.smith@gmail.com>
2017-06-12 15:47:16 -0700

    some styling and content for home page

--- src/main/resources/static/assets/js/script.js
+++ src/main/resources/static/assets/js/script.js
@@ -1,5 +1,12 @@
-function unhide(contentID) {
+function unhide(contentID, buttonID) {
     var content = document.getElementById(contentID);
+    if (content.classList.contains('hidden')) {
+        document.getElementById("content").style.width = "50%";
+        document.getElementById(buttonID).children[0].classList.add('selectedcat');
+    } else {
+        document.getElementById("content").style.width = "0%";
+        document.getElementById(buttonID).children[0].classList.remove('selectedcat');
+    }
     content.classList.toggle('unhidden');
     content.classList.toggle('hidden');
 
@@ -7,9 +14,11 @@ function unhide(contentID) {
     var all = document.getElementById("content").children;
     var i;
     for (i = 0; i < all.length; i++) {
-        if (all[i].id != contentID) {
+        if (all[i].id != contentID && all[i].tagName.toLowerCase() == 'div') {
             all[i].classList.add('hidden');
             all[i].classList.remove('unhidden');
+        } else if (all[i].id != buttonID && all[i].tagName.toLowerCase() == 'a') {
+            all[i].children[0].classList.remove('selectedcat')
         }
     }
 }
