503a1ec214d85b5e0ebed20b121aa88db0c28677
RebekahPerkins <r.perkins.smith@gmail.com>
2017-06-21 17:29:34 -0700

    Ready for wildfly

--- src/main/java/com/rebekahperkins/website/Application.java
+++ src/main/java/com/rebekahperkins/website/Application.java
@@ -2,12 +2,20 @@ package com.rebekahperkins.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan //(basePackages = "com.rebekahperkins.website")
-public class Application {
+public class Application extends SpringBootServletInitializer{
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(Application.class);
+  }
+
 }
